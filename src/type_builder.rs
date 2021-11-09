9762f67885dbae61667e37c286a5756ef732d89c
James Guthrie <jguthrie@timescale.com>
2021-11-09 15:56:43 +0100

    Fix type_builder

--- src/type_builder.rs
+++ src/type_builder.rs
@@ -13,40 +13,55 @@ macro_rules! do_serialize {
     ($state: ident, version: $version: expr) => {
         {
             use $crate::type_builder::SerializationType;
+            use std::io::{Cursor, Write};
+            use std::convert::TryInto;
 
             let state = &*$state;
             let serialized_size = bincode::serialized_size(state)
                 .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
-            let size = serialized_size + 2; // size of serialized data + our version flags
-            let mut bytes = Vec::with_capacity(size as usize + 4);
+            let our_size = serialized_size + 2; // size of serialized data + our version flags
+            let allocated_size = our_size + 4; // size of our data + the varlena header
+            let allocated_size = allocated_size.try_into()
+                .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
+            // valena tyes have a maximum size
+            if allocated_size > 0x3FFFFFFF {
+                pgx::error!("size {} bytes is to large", allocated_size)
+            }
+
+            let bytes: &mut [u8] = unsafe {
+                let bytes = pgx::pg_sys::palloc0(allocated_size);
+                std::slice::from_raw_parts_mut(bytes.cast(), allocated_size)
+            };
+            let mut writer = Cursor::new(bytes);
+            // varlena header space
             let varsize = [0; 4];
-            bytes.extend_from_slice(&varsize);
+            writer.write_all(&varsize)
+                .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
             // type version
-            bytes.push($version);
+            writer.write_all(&[$version])
+                .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
             // serialization version; 1 for bincode is currently the only option
-            bytes.push(SerializationType::Default as u8);
-            bincode::serialize_into(&mut bytes, state)
+            writer.write_all(&[SerializationType::Default as u8])
+                .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
+            bincode::serialize_into(&mut writer, state)
                 .unwrap_or_else(|e| pgx::error!("serialization error {}", e));
             unsafe {
-                ::pgx::set_varsize(bytes.as_mut_ptr() as *mut _, bytes.len() as i32);
+                let len = writer.position().try_into().expect("serialized size too large");
+                ::pgx::set_varsize(writer.get_mut().as_mut_ptr() as *mut _, len);
             }
-            bytea::from(bytes.leak().as_mut_ptr() as pg_sys::Datum)
+            writer.into_inner().as_mut_ptr() as pg_sys::Datum
         }
     };
 }
 #[macro_export]
 macro_rules! do_deserialize {
-    ($bytes: expr, $t: ty) => {{
-        use std::slice;
+    ($bytes: ident, $t: ty) => {{
         use $crate::type_builder::SerializationType;
 
         let state: $t = unsafe {
-            let input: bytea = $bytes;
-            let input: pgx::pg_sys::Datum = input.into();
-            let detoasted = pg_sys::pg_detoast_datum_packed(input as *mut _);
+            let detoasted = pg_sys::pg_detoast_datum_packed($bytes as *mut _);
             let len = pgx::varsize_any_exhdr(detoasted);
             let data = pgx::vardata_any(detoasted);
-
             let bytes = slice::from_raw_parts(data as *mut u8, len);
             if bytes.len() < 1 {
                 pgx::error!("deserialization error, no bytes")
