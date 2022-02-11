4ff9ca77331159c93f3ac1618f960b47e608cdfb
James Guthrie <jguthrie@timescale.com>
2022-02-11 14:29:02 +0100

    Remove estimate adjustment from Rust SQL
    It appears as though these estimate adjustments are already present in
    the hand-written SQL, so are superfluous.

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -1,6 +1,5 @@
 //! This module serves to provide additional schema generation which is not
 //! directly related to any functionality which this extension itself provides.
 
-mod estimates;
 mod setup;
 mod sql;
