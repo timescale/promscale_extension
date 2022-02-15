753b07da0294a5901e2e63a018a4cf494906a5a9
James Guthrie <jguthrie@timescale.com>
2022-02-15 19:08:51 +0100

    WIP: create version upgrade symlinks
    Note: some assembly required.
    
    Run:
    ```
    cargo pgx schema pg14
    mv sql/promscale-0.5.0.sql sql/promscale--0.5.0.sql
    ./create-upgrade-symlinks
    make docker-image-ts2-14
    ```

---
+++ create-upgrade-symlinks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+prev_versions=$(cat promscale.control | sed -n 's/# upgradeable_from = \(.*\)/\1/p' | sed "s/[[:space:]']//g" | tr ',' '\n')
+cur_version=$(cargo pkgid | cut -d'#' -f2 | cut -d':' -f2)
+
+for prev_version in $prev_versions; do
+  if [ -n "${prev_version}" ] && [ -n "${cur_version}" ]; then
+    ln -s -f "promscale--${cur_version}.sql" "sql/promscale--${prev_version}--${cur_version}.sql"
+  fi
+done
