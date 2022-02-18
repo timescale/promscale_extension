f7410a48640781206f6a131be949420ee9272a5b
John Pruitt <jgpruitt@gmail.com>
2022-02-18 15:57:49 -0600

    fixed create-upgrade-symlinks.sh so that the upgrade scripts are created

--- create-upgrade-symlinks.sh
+++ create-upgrade-symlinks.sh
@@ -3,8 +3,9 @@
 prev_versions=$(cat promscale.control | sed -n 's/# upgradeable_from = \(.*\)/\1/p' | sed "s/[[:space:]']//g" | tr ',' '\n')
 cur_version=$(cargo pkgid | cut -d'#' -f2 | cut -d':' -f2)
 
+cd sql
 for prev_version in $prev_versions; do
   if [ -n "${prev_version}" ] && [ -n "${cur_version}" ]; then
-    ln -s -f "promscale--${cur_version}.sql" "sql/promscale--${prev_version}--${cur_version}.sql"
+    ln -s -f "promscale--${cur_version}.sql" "promscale--${prev_version}--${cur_version}.sql"
   fi
 done
