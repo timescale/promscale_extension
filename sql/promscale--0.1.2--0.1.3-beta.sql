1a22138c99b53dc09366bb61870c2902283dce82
James Guthrie <jguthrie@timescale.com>
2022-02-24 15:57:01 +0100

    Fix privilege escalation
    This change fixes a privilege escalation vulnerability, by which an
    unprivileged user precreates a function or operator which is later
    executed from a privileged context.
    
    Additionally, this change uses the fully-qualified name for functions,
    tables, and operators which are used throughout the extension.

--- sql/promscale--0.1.2--0.1.3-beta.sql
+++ sql/promscale--0.1.2--0.1.3-beta.sql
@@ -1,3 +1,5 @@
+SET LOCAL search_path = pg_catalog;
+
 CREATE OR REPLACE FUNCTION @extschema@.prom_delta_transition(state internal, lowest_time timestamptz,
     greatest_time timestamptz, step bigint, range bigint,
     sample_time timestamptz, sample_value double precision)
@@ -21,15 +23,15 @@ RETURNS DOUBLE PRECISION[]
 AS '$libdir/promscale', 'prom_delta_final_wrapper'
 LANGUAGE C IMMUTABLE PARALLEL SAFE;
 
-CREATE OR REPLACE FUNCTION @extschema@."vector_selector_transition"("state" internal, "start_time" TimestampTz, "end_time" TimestampTz, "bucket_width" bigint, "lookback" bigint, "time" TimestampTz, "val" double precision) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_transition_wrapper';
+CREATE FUNCTION @extschema@."vector_selector_transition"("state" internal, "start_time" TimestampTz, "end_time" TimestampTz, "bucket_width" bigint, "lookback" bigint, "time" TimestampTz, "val" double precision) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_transition_wrapper';
 -- ./src/lib.rs:337:0
-CREATE OR REPLACE FUNCTION @extschema@."vector_selector_final"("state" internal) RETURNS double precision[] IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_final_wrapper';
+CREATE FUNCTION @extschema@."vector_selector_final"("state" internal) RETURNS double precision[] IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_final_wrapper';
 -- ./src/lib.rs:345:0
-CREATE OR REPLACE FUNCTION @extschema@."vector_selector_serialize"("state" internal) RETURNS bytea IMMUTABLE STRICT PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_serialize_wrapper';
+CREATE FUNCTION @extschema@."vector_selector_serialize"("state" internal) RETURNS bytea IMMUTABLE STRICT PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_serialize_wrapper';
 -- ./src/lib.rs:350:0
-CREATE OR REPLACE FUNCTION @extschema@."vector_selector_deserialize"("bytes" bytea, "_internal" internal) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_deserialize_wrapper';
+CREATE FUNCTION @extschema@."vector_selector_deserialize"("bytes" bytea, "_internal" internal) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_deserialize_wrapper';
 -- ./src/lib.rs:358:0
-CREATE OR REPLACE FUNCTION @extschema@."vector_selector_combine"("state1" internal, "state2" internal) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_combine_wrapper';
+CREATE FUNCTION @extschema@."vector_selector_combine"("state1" internal, "state2" internal) RETURNS internal IMMUTABLE PARALLEL SAFE LANGUAGE c AS 'MODULE_PATHNAME', 'vector_selector_combine_wrapper';
 CREATE AGGREGATE @extschema@.vector_selector(
     start_time timestamptz,
     end_time timestamptz,
