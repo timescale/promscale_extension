2381ec668592de28c0c6ea1b8daead6be079e78e
James Guthrie <jguthrie@timescale.com>
2022-04-01 09:21:14 +0200

    Add test to CREATE and DROP extension

--- e2e/tests/test.rs
+++ e2e/tests/test.rs
@@ -15,3 +15,20 @@ fn create_promscale_extension() {
 
     assert_eq!(result.len(), 1);
 }
+
+#[test]
+#[ignore]
+fn create_drop_promscale_extension() {
+    let _ = pretty_env_logger::try_init();
+
+    let docker = clients::Cli::default();
+    let node = run_postgres(&docker);
+
+    let mut client = common::connect(&node);
+    let result = client.simple_query("CREATE EXTENSION promscale;").unwrap();
+    assert_eq!(result.len(), 1);
+
+    let result = client.simple_query("DROP EXTENSION promscale;").unwrap();
+
+    assert_eq!(result.len(), 1);
+}
