7f4be2017e6040b365be3c945fc4cc4dac55920d
John Pruitt <jgpruitt@gmail.com>
2022-04-20 21:53:55 -0500

    dynamically generate meta commands to describe objects in the schemas

--- e2e/scripts/snapshot.sql
+++ e2e/scripts/snapshot.sql
@@ -8,101 +8,35 @@ select version();
 \dn
 \dx+ promscale
 
--- tables
-\d+ public.*
-\d+ _prom_catalog.*
-\d+ _prom_ext.*
-\d+ _ps_catalog.*
-\d+ _ps_trace.*
-\d+ prom_api.*
-\d+ prom_data.*
-\d+ prom_data_exemplar.*
-\d+ prom_data_series.*
-\d+ prom_info.*
-\d+ prom_metric.*
-\d+ prom_series.*
-\d+ ps_tag.*
-\d+ ps_trace.*
-
--- functions
-\df+ public.*
-\df+ _prom_catalog.*
-\df+ _prom_ext.*
-\df+ _ps_catalog.*
-\df+ _ps_trace.*
-\df+ prom_api.*
-\df+ prom_data.*
-\df+ prom_data_exemplar.*
-\df+ prom_data_series.*
-\df+ prom_info.*
-\df+ prom_metric.*
-\df+ prom_series.*
-\df+ ps_tag.*
-\df+ ps_trace.*
-
--- privileges
-\dp+ public.*
-\dp+ _prom_catalog.*
-\dp+ _prom_ext.*
-\dp+ _ps_catalog.*
-\dp+ _ps_trace.*
-\dp+ prom_api.*
-\dp+ prom_data.*
-\dp+ prom_data_exemplar.*
-\dp+ prom_data_series.*
-\dp+ prom_info.*
-\dp+ prom_metric.*
-\dp+ prom_series.*
-\dp+ ps_tag.*
-\dp+ ps_trace.*
-
--- indicies
-\di public.*
-\di _prom_catalog.*
-\di _prom_ext.*
-\di _ps_catalog.*
-\di _ps_trace.*
-\di prom_api.*
-\di prom_data.*
-\di prom_data_exemplar.*
-\di prom_data_series.*
-\di prom_info.*
-\di prom_metric.*
-\di prom_series.*
-\di ps_tag.*
-\di ps_trace.*
-
--- triggers
-\dy public.*
-\dy _prom_catalog.*
-\dy _prom_ext.*
-\dy _ps_catalog.*
-\dy _ps_trace.*
-\dy prom_api.*
-\dy prom_data.*
-\dy prom_data_exemplar.*
-\dy prom_data_series.*
-\dy prom_info.*
-\dy prom_metric.*
-\dy prom_series.*
-\dy ps_tag.*
-\dy ps_trace.*
-
--- operators
-\do public.*
-\do _prom_catalog.*
-\do _prom_ext.*
-\do _ps_catalog.*
-\do _ps_trace.*
-\do prom_api.*
-\do prom_data.*
-\do prom_data_exemplar.*
-\do prom_data_series.*
-\do prom_info.*
-\do prom_metric.*
-\do prom_series.*
-\do ps_tag.*
-\do ps_trace.*
+-- dynamically generate meta commands to describe objects in the schemas
+select format('%s %s', c.c, s.s)
+from unnest(array[
+    'public.*',
+    '_prom_catalog.*',
+    '_prom_ext.*',
+    '_ps_catalog.*',
+    '_ps_trace.*',
+    'prom_api.*',
+    'prom_data.*',
+    'prom_data_exemplar.*',
+    'prom_data_series.*',
+    'prom_info.*',
+    'prom_metric.*',
+    'prom_series.*',
+    'ps_tag.*',
+    'ps_trace.*'
+]) s(s)
+cross join unnest(array[
+    '\d+',
+    '\df+',
+    '\dp+',
+    '\di',
+    '\dy',
+    '\do',
+    '\dT'
+]) c(c)
+\g (tuples_only=on format=csv) describe.sql
+\i describe.sql
 
 -- snapshot the data from all the tables
 select
