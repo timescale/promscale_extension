6dbdd20c6ee2abc9b604dda2e7028ab625ae43f6
John Pruitt <jgpruitt@gmail.com>
2022-04-25 14:43:45 -0500

    add event and link pre-dump

--- e2e/scripts/post-restore.sql
+++ e2e/scripts/post-restore.sql
@@ -1,4 +1,4 @@
 select public.timescaledb_post_restore();
 set timescaledb.restoring to 'off'; -- timescaledb bug https://github.com/timescale/timescaledb/issues/4267
-select _timescaledb_internal.restart_background_workers();
+select _timescaledb_internal.restart_background_workers(); -- timescaledb bug https://github.com/timescale/timescaledb/issues/4267
 select public.promscale_post_restore();
