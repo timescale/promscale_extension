f693e24de1c441740441ee7de2169cbfa8776850
James Guthrie <jguthrie@timescale.com>
2022-06-09 09:41:57 +0200

    Add end-to-end test for version upgrade

--- e2e/tests/test.rs
+++ e2e/tests/test.rs
@@ -20,3 +20,22 @@ fn create_drop_promscale_extension() {
 
     assert_eq!(result.len(), 1);
 }
+
+#[test]
+fn upgrade_promscale_extension_0_5_0_to_0_5_1() {
+    let _ = pretty_env_logger::try_init();
+
+    let docker = clients::Cli::default();
+    let node = run_postgres(&docker);
+
+    let mut client = common::connect(&node);
+    let result = client
+        .simple_query("CREATE EXTENSION promscale VERSION '0.5.0';")
+        .unwrap();
+    assert_eq!(result.len(), 1);
+
+    let result = client
+        .simple_query("ALTER EXTENSION promscale UPDATE TO '0.5.1';")
+        .unwrap();
+    assert_eq!(result.len(), 1);
+}
