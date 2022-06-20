0bb102082479438f67df3f8bfa1c1bb388f657d0
James Guthrie <jguthrie@timescale.com>
2022-06-20 17:41:53 +0200

    Prepare for the next development cycle

--- e2e/tests/incremental-freeze-test.rs
+++ e2e/tests/incremental-freeze-test.rs
@@ -21,7 +21,7 @@ fn incremental_freeze_test() {
 
     let frozen_files = [
         // ↓↓↓ frozen in 0.5.0 ↓↓↓
-        ("001-extension.sql", "80774ee806662753d605908ca449c8b4"),
+        ("001-extension.sql", "240674baa0d497b17a96a4fe1e043381"),
         ("002-utils.sql", "2450a0291c64f48e80bd4d4638f3bba0"),
         ("003-users.sql", "ca921c533531d5715bfeb688f569325f"),
         ("004-schemas.sql", "f2785b92611bd621c8fb64f2a5403b47"),
---
+++ sql/promscale--0.5.0--0.5.2.sql
@@ -0,0 +1,1 @@
+promscale--0.5.2.sql
