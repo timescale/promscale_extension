51caf8459485099e4859b65e8117af2b50967d5b
niksajakovljevic <niksa.jakovljevic@gmail.com>
2022-07-07 16:00:36 +0200

    Pass prefix for temporary table used for ingest
    It can happen that we insert both samples and examplars within the same
    transaction. We need a separate temp table for these two so passing a prefix
    shoud help with that.

--- e2e/testdata/create_ingest_temp_table.sql
+++ e2e/testdata/create_ingest_temp_table.sql
@@ -4,16 +4,16 @@
 
 CREATE EXTENSION promscale;
 
-CREATE TEMP TABLE metric_names(name TEXT);
+CREATE TEMP TABLE test_data(metric_name TEXT, prefix TEXT);
 
-INSERT INTO metric_names VALUES
-  ('cpu_usage'),
-  ('aMultiCase"fun"Metric!'),
-  ('aVeryLongMultiCaseMetricWhich"might"BeTruncatedIfIt''sTOOOOOOOOOOOOOOOLONGZOMG!');
+INSERT INTO test_data VALUES
+  ('cpu_usage', 'exe_'),
+  ('aMultiCase"fun"Metric!', 'eXe_'),
+  ('aVeryLongMultiCaseMetricWhich"might"BeTruncatedIfIt''sTOOOOOOOOOOOOOOOLONGZOMG!','ThisIsLong_');
 
 -- Note: Metric creation must be outside of the test function below, to avoid
 -- 'invalid transaction termination'
-SELECT _prom_catalog.get_or_create_metric_table_name(name) FROM metric_names;
+SELECT _prom_catalog.get_or_create_metric_table_name(metric_name) FROM test_data;
 CALL _prom_catalog.finalize_metric_creation();
 
 CREATE OR REPLACE FUNCTION test_create_ingest_temp_table()
@@ -21,17 +21,17 @@ RETURNS SETOF TEXT LANGUAGE plpgsql AS $$
 DECLARE
     metric_table_name TEXT;
     temp_table_name TEXT;
-    metric_name TEXT;
     temp_schema TEXT;
+    test RECORD;
 BEGIN
-    FOR metric_name IN SELECT name FROM metric_names
+    FOR test IN SELECT t.metric_name, t.prefix FROM test_data t
     LOOP
-        SELECT table_name INTO metric_table_name FROM _prom_catalog.get_or_create_metric_table_name(metric_name);
+        SELECT table_name INTO metric_table_name FROM _prom_catalog.get_or_create_metric_table_name(test.metric_name);
 
         RETURN NEXT has_table('prom_data', metric_table_name, format('table %I.%I exists', 'prom_data', metric_table_name));
 
-        SELECT _prom_catalog.create_ingest_temp_table(metric_table_name, 'prom_data') INTO temp_table_name;
-        RETURN NEXT is(temp_table_name, metric_table_name, 'temp table name is well-formed');
+        SELECT _prom_catalog.create_ingest_temp_table(metric_table_name, 'prom_data', test.prefix) INTO temp_table_name;
+        RETURN NEXT is(temp_table_name, left(test.prefix || metric_table_name, 62) , 'temp table name is well-formed');
         SELECT nspname INTO temp_schema FROM pg_namespace WHERE oid = pg_my_temp_schema();
         RETURN NEXT has_table(temp_schema, temp_table_name, format('temp table %I exists', temp_table_name));
     END LOOP;
