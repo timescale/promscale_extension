443066253ca824abb773af0f7e6cb7ac5d625bf2
John Pruitt <jgpruitt@gmail.com>
2022-09-13 16:33:44 -0500

    Fix and simplify chunks_to_vacuum_freeze
    relallvisible is a count, not a boolean.
    order by relfrozenxid to remove a bunch of joins

--- migration/idempotent/016-vacuum-engine.sql
+++ migration/idempotent/016-vacuum-engine.sql
@@ -1,40 +1,29 @@
 DO $block$
 BEGIN
     IF _prom_catalog.is_timescaledb_installed() THEN
-        CREATE OR REPLACE VIEW _ps_catalog.chunks_to_vacuum_freeze AS
+        CREATE OR REPLACE VIEW _ps_catalog.chunks_to_freeze AS
         SELECT
-            c.hypertable_id,
-            c.id as chunk_id,
-            cc.id as compressed_chunk_id,
+            cc.id,
             cc.schema_name,
             cc.table_name,
-            t.vacuum_count,
-            t.autovacuum_count,
-            t.last_vacuum,
-            t.last_autovacuum,
-            k.relallvisible
+            greatest
+            (
+                pg_catalog.pg_stat_get_last_vacuum_time(k.oid),
+                pg_catalog.pg_stat_get_last_autovacuum_time(k.oid)
+            ) as last_vacuum,
+            k.relpages,
+            k.relallvisible,
+            k.relfrozenxid
         FROM _timescaledb_catalog.chunk c
-        INNER JOIN _timescaledb_catalog.chunk cc ON (c.dropped = false and c.compressed_chunk_id = cc.id)
-        INNER JOIN
-        (
-            SELECT
-              d.id,
-              d.hypertable_id,
-              row_number() OVER (PARTITION BY d.hypertable_id ORDER BY d.id) as dimension_nbr
-            FROM _timescaledb_catalog.dimension d
-        ) d ON (c.hypertable_id = d.hypertable_id and d.dimension_nbr = 1)
-        INNER JOIN _timescaledb_catalog.dimension_slice ds on (d.id = ds.dimension_id)
-        INNER JOIN _timescaledb_catalog.chunk_constraint con on (ds.id = con.dimension_slice_id and con.chunk_id = c.id)
-        INNER JOIN pg_class k ON (k.oid = format('%I.%I', cc.schema_name, cc.table_name)::regclass::oid)
-        INNER JOIN pg_stat_all_tables t ON (t.relid = k.oid)
-        WHERE k.relallvisible = 0 -- not already fully frozen
-        ORDER BY
-            greatest(t.vacuum_count, t.autovacuum_count), -- chunks vacuumed fewest times first
-            greatest(t.last_vacuum, t.last_autovacuum) NULLS FIRST, -- chunks never vacuumed first followed by ones vacuumed furthest in the past
-            ds.range_start -- chunks that represent older data first
+        INNER JOIN _timescaledb_catalog.chunk cc
+        ON (c.dropped OPERATOR(pg_catalog.=) false and c.compressed_chunk_id OPERATOR(pg_catalog.=) cc.id)
+        INNER JOIN pg_catalog.pg_class k
+        ON (k.oid OPERATOR(pg_catalog.=) format('%I.%I', cc.schema_name, cc.table_name)::regclass::oid)
+        WHERE k.relallvisible OPERATOR(pg_catalog.<) k.relpages
+        ORDER BY pg_catalog.age(k.relfrozenxid) DESC
         ;
-        GRANT SELECT ON _ps_catalog.chunks_to_vacuum_freeze TO prom_reader;
-        COMMENT ON VIEW _ps_catalog.chunks_to_vacuum_freeze IS 'Lists chunks that need to be vacuumed and frozen';
+        GRANT SELECT ON _ps_catalog.chunks_to_freeze TO prom_reader;
+        COMMENT ON VIEW _ps_catalog.chunks_to_freeze IS 'Lists compressed chunks that need to be frozen';
     END IF;
 END;
 $block$;
--- sql-tests/testdata/chunks_to_vacuum_freeze.sql
+++ sql-tests/testdata/chunks_to_vacuum_freeze.sql
@@ -2,19 +2,10 @@
 \set QUIET 1
 \i 'testdata/scripts/pgtap-1.2.0.sql'
 
-SELECT * FROM plan(8);
-
-/*
-    create two hypertables and load them with enough data to create a couple thousand chunks
-    enable compression on the hypertables
-    but set the interval high enough that compression doesn't happen automatically
-    we will compress a few, and see if they show up in the view
-    then, we'll vacuum freeze them and make sure they disappear from the view
-    rinse repeat
-*/
+select * from plan(5);
 
 -- create one hypertable
-create table metric1(id int, t timestamptz not null, val double precision);
+create table metric1(id int, t timestamptz not null, val double precision) with (autovacuum_enabled = 'off');
 select create_hypertable('metric1'::regclass, 't', chunk_time_interval=>'5 minutes'::interval);
 alter table metric1 set (timescaledb.compress, timescaledb.compress_segmentby = 'id');
 select add_compression_policy('metric1', INTERVAL '7 days');
@@ -26,11 +17,11 @@ select format($$insert into metric1 (id, t, val) values (%L, %L, %L)$$
 , random()
 )
 from generate_series(1, 10) id
-cross join generate_series(1, 1000) t
+cross join generate_series(1, 50) t
 \gexec
 
 -- create a second hypertable
-create table metric2(id int, t timestamptz not null, val double precision);
+create table metric2(id int, t timestamptz not null, val double precision) with (autovacuum_enabled = 'off');
 select create_hypertable('metric2'::regclass, 't', chunk_time_interval=>'5 minutes'::interval);
 alter table metric2 set (timescaledb.compress, timescaledb.compress_segmentby = 'id');
 select add_compression_policy('metric2', INTERVAL '7 days');
@@ -42,30 +33,48 @@ select format($$insert into metric2 (id, t, val) values (%L, %L, %L)$$
 , random()
 )
 from generate_series(1, 10) id
-cross join generate_series(1, 1000) t
+cross join generate_series(1, 50) t
 \gexec
 
 -- make sure we got some chunks out of the endeavor
-select isnt_empty('select * from _timescaledb_catalog.chunk', 'chunks were created');
+select isnt_empty(
+    'select * from _timescaledb_catalog.chunk',
+    'chunks were created'
+);
 
 -- there ought not be any results from the view because no chunks are compressed
-select is_empty('select * from _ps_catalog.chunks_to_vacuum_freeze', 'zero results when no chunks compressed');
+select is_empty(
+    'select * from _ps_catalog.chunks_to_freeze',
+    'zero results when no chunks compressed'
+);
+
+-- disable autovacuum on the compressed hypertable too so the compressed chunks won't
+-- get vacuumed automatically either
+select format($$alter table %I.%I set (autovacuum_enabled = 'off')$$, ch.schema_name, ch.table_name)
+from _timescaledb_catalog.hypertable h
+inner join _timescaledb_catalog.hypertable ch on (h.compressed_hypertable_id = ch.id)
+and h.schema_name = 'public' and h.table_name in ('metric1', 'metric2')
+\gexec
+
 
 create temp table _chosen_uncompressed_chunks (id int, schema_name text, table_name text);
 create temp table _chosen_compressed_chunks (id int, schema_name text, table_name text);
 
--- pick 5 random uncompressed chunks
+-- pick 15 random uncompressed chunks and compress them
 insert into pg_temp._chosen_uncompressed_chunks (id, schema_name, table_name)
 select id, schema_name, table_name
 from _timescaledb_catalog.chunk k
 where compressed_chunk_id is null
+and table_name not like 'compress_%'
 order by random()
-limit 5
+limit 15
 ;
+
+-- make sure we got 15
 select results_eq(
     'select count(*) from pg_temp._chosen_uncompressed_chunks',
-    array[5::bigint],
-    '5 uncompressed chunks should have been chosen'
+    array[15::bigint],
+    '15 uncompressed chunks should have been chosen'
 );
 
 -- compress the chosen chunks
@@ -85,7 +94,7 @@ begin
 end;
 $block$;
 
-
+-- find the chunks we compressed
 insert into _chosen_compressed_chunks (id, schema_name, table_name)
 select cc.id, cc.schema_name, cc.table_name
 from _chosen_uncompressed_chunks x
@@ -93,76 +102,29 @@ inner join _timescaledb_catalog.chunk c on (x.id = c.id)
 inner join _timescaledb_catalog.chunk cc on (c.compressed_chunk_id = cc.id)
 ;
 
--- view should return the 5 chosen compressed chunks
+-- make sure we got 15
 select results_eq(
-    'select compressed_chunk_id from _ps_catalog.chunks_to_vacuum_freeze order by compressed_chunk_id',
+    'select count(*) from pg_temp._chosen_compressed_chunks',
+    array[15::bigint],
+    '15 compressed chunks should have been created'
+);
+
+-- view should return the 15 chosen compressed chunks
+select results_eq(
+    'select id from _ps_catalog.chunks_to_freeze order by id',
     'select id from pg_temp._chosen_compressed_chunks order by id',
-    'view should return the 5 chosen compressed chunks'
+    'view should return the 15 chosen compressed chunks'
 );
 
--- vacuum freeze the chosen
-select format('vacuum (freeze, analyze) %I.%I', schema_name, table_name)
-from pg_temp._chosen_compressed_chunks
-\gexec
+/*
+here we *could* vacuum (freeze, analyze) each of the compressed chunks
+and see whether they disappear from the view
+unfortunately, it appears that the relallvisible column is not updated
+transactionally or immediately with the vacuum
+so it becomes a matter of pg_sleeping an indeterminate amount of time
+and hoping that the column has been updated by the time we check the view
+this leads to extremely flaky tests and much wailing and gnashing of teeth
+so, regretfully, we are not doing that here
+*/
 
-select is_empty('select * from _ps_catalog.chunks_to_vacuum_freeze', 'zero results after chunks were frozen');
-
--- reset and do it again ------------------------------------------------------
-truncate _chosen_uncompressed_chunks;
-truncate _chosen_compressed_chunks;
--------------------------------------------------------------------------------
-
--- pick 5 random uncompressed chunks
-insert into pg_temp._chosen_uncompressed_chunks (id, schema_name, table_name)
-select id, schema_name, table_name
-from _timescaledb_catalog.chunk k
-where compressed_chunk_id is null
-order by random()
-limit 5
-;
-select results_eq(
-    'select count(*) from pg_temp._chosen_uncompressed_chunks',
-    array[5::bigint],
-    '5 uncompressed chunks should have been chosen'
-);
-
--- compress the chosen chunks
-do $block$
-declare
-    _sql text;
-begin
-    for _sql in
-    (
-        select format('select compress_chunk(%L::regclass)', format('%s.%s', schema_name, table_name))
-        from pg_temp._chosen_uncompressed_chunks
-        order by id
-    )
-    loop
-        execute _sql;
-    end loop;
-end;
-$block$;
-
-
-insert into _chosen_compressed_chunks (id, schema_name, table_name)
-select cc.id, cc.schema_name, cc.table_name
-from _chosen_uncompressed_chunks x
-inner join _timescaledb_catalog.chunk c on (x.id = c.id)
-inner join _timescaledb_catalog.chunk cc on (c.compressed_chunk_id = cc.id)
-;
-
--- view should return the 5 chosen compressed chunks
-select results_eq(
-    'select compressed_chunk_id from _ps_catalog.chunks_to_vacuum_freeze order by compressed_chunk_id',
-    'select id from pg_temp._chosen_compressed_chunks order by id',
-    'view should return the 5 chosen compressed chunks'
-);
-
--- vacuum freeze the chosen
-select format('vacuum (freeze, analyze) %I.%I', schema_name, table_name)
-from pg_temp._chosen_compressed_chunks
-\gexec
-
-select is_empty('select * from _ps_catalog.chunks_to_vacuum_freeze', 'zero results after chunks were frozen');
-
-SELECT * FROM finish(true);
+select * from finish(true);
