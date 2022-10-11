a199e463c53de83e84b4cbc67f96a9f7020a57a4
Valery Meleshkin <valeriy@timescale.com>
2022-10-11 19:34:35 +0200

    updating the "prior" images

--- e2e/tests/config/mod.rs
+++ e2e/tests/config/mod.rs
@@ -3,8 +3,8 @@
 pub(crate) const ALPINE_WITH_EXTENSION_LAST_RELEASED_PREFIX: &str =
     "timescaledev/promscale-extension:0.7.0-ts2.7.2-pg";
 pub(crate) const HA_WITH_LAST_RELEASED_EXTENSION_PG14: &str =
-    "timescale/timescaledb-ha:pg14.5-ts2.8.1-p0";
+    "timescale/timescaledb-ha:pg14.5-ts2.8.1-p1";
 pub(crate) const HA_WITH_LAST_RELEASED_EXTENSION_PG13: &str =
-    "timescale/timescaledb-ha:pg13.8-ts2.8.1-p0";
+    "timescale/timescaledb-ha:pg13.8-ts2.8.1-p1";
 pub(crate) const HA_WITH_LAST_RELEASED_EXTENSION_PG12: &str =
-    "timescale/timescaledb-ha:pg12.12-ts2.8.1-p0";
+    "timescale/timescaledb-ha:pg12.12-ts2.8.1-p1";
