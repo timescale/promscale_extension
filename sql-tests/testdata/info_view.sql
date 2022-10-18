5eb81efc93159c61ee75a68a5b46d1c189f18e7b
John Pruitt <jgpruitt@gmail.com>
2022-10-18 11:09:51 -0500

    Optimize the prom_info.metric view
    prom_info.metric can be quite slow. Part of this is due to the columns
    that deal with disk size. Split the view into two views
        - one that does not have the size columns and is optimized for
          speed
        - and the original (also optimized for better performance)
    
    prom_info.metric is now a SQL view (not backed by a function) and it
    omits the columns dealing with disk size.
    
    prom_info.metric_detail is a view that is backed by a function
    (_prom_catalog.metric_detail) which has the same signature as the
    original view. i.e. it contains the columns dealing with disk size. If
    this is a multinode installation (which we don't currently even
    support), it executes the original query. If this is a singlenode
    installation (which should be everyone), we use a new optimized query.

--- sql-tests/testdata/info_view.sql
+++ sql-tests/testdata/info_view.sql
@@ -17,14 +17,74 @@ INSERT INTO prom_data.cpu_total
 SELECT timestamptz '2000-01-01 02:03:04'+(interval '1s' * g), 100.0, _prom_catalog.get_or_create_series_id('{"__name__": "cpu_total", "namespace":"production", "node": "pinky", "new_tag_2":"bar"}')
 FROM generate_series(1,10) g;
 
-SELECT id , metric_name , table_name, retention_period, chunk_interval > interval '7 hour', compressed_interval > interval '7 hour', before_compression_bytes, after_compression_bytes, label_keys, total_size, total_size_bytes, compression_ratio, total_chunks, compressed_chunks FROM prom_info.metric ORDER BY id;
+SELECT
+    id,
+    metric_name,
+    table_name,
+    label_keys,
+    retention_period,
+    chunk_interval > interval '7 hours',
+    compressed_interval > interval '7 hours',
+    total_chunks,
+    compressed_chunks
+FROM prom_info.metric
+ORDER BY id;
+
+SELECT
+    id,
+    metric_name,
+    table_name,
+    retention_period,
+    chunk_interval > interval '7 hour',
+    compressed_interval > interval '7 hour',
+    before_compression_bytes,
+    after_compression_bytes,
+    label_keys,
+    total_size,
+    total_size_bytes,
+    compression_ratio,
+    total_chunks,
+    compressed_chunks
+FROM prom_info.metric_detail
+ORDER BY id;
 -- compress chunks
 SELECT public.compress_chunk(public.show_chunks('prom_data.cpu_usage'));
 SELECT public.compress_chunk(public.show_chunks('prom_data.cpu_total'));
 -- fetch stats with compressed chunks
 
 SET ROLE prom_reader;
-SELECT id , metric_name , table_name, retention_period, chunk_interval > interval '7 hour', compressed_interval > interval '7 hour', before_compression_bytes, after_compression_bytes, label_keys, total_size, total_size_bytes, compression_ratio, total_chunks, compressed_chunks FROM prom_info.metric ORDER BY id;
+
+SELECT
+    id,
+    metric_name,
+    table_name,
+    label_keys,
+    retention_period,
+    chunk_interval > interval '7 hours',
+    compressed_interval > interval '7 hours',
+    total_chunks,
+    compressed_chunks
+FROM prom_info.metric
+ORDER BY id;
+
+SELECT
+    id,
+    metric_name,
+    table_name,
+    retention_period,
+    chunk_interval > interval '7 hour',
+    compressed_interval > interval '7 hour',
+    before_compression_bytes,
+    after_compression_bytes,
+    label_keys,
+    total_size,
+    total_size_bytes,
+    compression_ratio,
+    total_chunks,
+    compressed_chunks
+FROM prom_info.metric_detail
+ORDER BY id;
+
 SELECT * FROM prom_info.label ORDER BY key;
 SELECT * FROM prom_info.metric_stats ORDER BY num_series_approx;
 SELECT * FROM prom_info.system_stats;
