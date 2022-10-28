dadeb13d9f91a9d38713cd8fdb0bb1e1dbdfcd5c
Harkishen-Singh <harkishensingh@hotmail.com>
2022-10-28 16:09:39 +0530

    Add tests for creation/deletion of metric-rollups.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- migration/incremental/033-metric-rollup.sql
+++ migration/incremental/033-metric-rollup.sql
@@ -1,19 +1,21 @@
 CREATE TABLE _prom_catalog.rollup (
-    id             SERIAL,
-    name           TEXT NOT NULL PRIMARY KEY,
+    id             SERIAL PRIMARY KEY,
+    name           TEXT NOT NULL,
     schema_name    TEXT NOT NULL,
     resolution     INTERVAL NOT NULL,
-    retention      INTERVAL NOT NULL
+    retention      INTERVAL NOT NULL,
+    UNIQUE(name)
 );
 GRANT SELECT ON TABLE _prom_catalog.rollup TO prom_reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE _prom_catalog.rollup TO prom_writer;
 
 CREATE TABLE _prom_catalog.metric_rollup (
-    id              SERIAL,
+    id              SERIAL PRIMARY KEY,
     rollup_id       INTEGER NOT NULL,
     metric_id       INTEGER NOT NULL,
     refresh_pending BOOLEAN DEFAULT TRUE NOT NULL,
-    PRIMARY KEY     (rollup_id, metric_id)
+    UNIQUE (rollup_id, metric_id),
+    FOREIGN KEY (rollup_id) REFERENCES _prom_catalog.rollup(id)
 );
 GRANT SELECT ON TABLE _prom_catalog.metric_rollup TO prom_reader;
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE _prom_catalog.metric_rollup TO prom_writer;
